dbb2411fb5a511246d376b7244320865551ece06
Rah <visoriano106@gmail.com>
2025-07-12 09:02:53 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
